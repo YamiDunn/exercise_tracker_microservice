881a77c80a52bd5f6ea13dcfcdb927dbefb5a921
YamiDunn <j.felipe.rodriguez@hotmail.com>
2023-04-04 18:07:48 -0500

    fixed, date issues

--- index.js
+++ index.js
@@ -24,54 +24,46 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const user = [{ username: "TestMain, _id", _id: 00000000000000000000 }];
+const user = [];
 const getTime = (date) => {
   let dateObj;
+  let tempDate;
   let validDate =
     /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/.test(
       date
     );
+
   if (date) {
     if (validDate) {
-      dateObj = new Date(date);
-      dateObj =
-        dateObj.getFullYear() +
-        "-" +
-        (dateObj.getMonth() + 1) +
-        "-" +
-        (dateObj.getDate() + 1);
-      console.log("you are here", dateObj);
+      tempDate = new Date(date);
+      tempDate.setDate(tempDate.getDate() + 1);
+      dateObj = tempDate.toDateString();
     } else {
       console.log("invalid date input");
     }
   } else {
     date = new Date();
-    dateObj =
-      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    dateObj = date.toDateString();
   }
   return dateObj;
 };
+app.route("/api/users").post((req, res) => {
+  let userData = req.body.username;
+  let gId = nanoid(20);
+  var cUser = { username: userData, _id: gId };
+  user.push(cUser);
+  res.json({ username: userData, _id: gId });
+});
 
-app
-  .route("/api/users")
-  .post((req, res, next) => {
-    let userData = req.body.username;
-    let gId = nanoid(20);
-    var cUser = { username: userData, _id: gId };
-    user.push(cUser);
-    res.json({ username: userData, _id: gId });
-    next();
-  })
-  .get((req, res) => {
-    res.json(user);
-  });
+app.get("/api/users", (req, res) => {
+  res.json(user);
+});
 
 app.route("/api/users/:_id/exercises").post((req, res) => {
   let uid = req.body[":_id"];
   let desc = req.body.description;
   let dur = req.body.duration;
   let date = req.body.date;
-
   try {
     let index = user.findIndex((el) => el._id == uid);
     if (index) {
@@ -79,11 +71,10 @@ app.route("/api/users/:_id/exercises").post((req, res) => {
       user[index].duration = dur;
       let dateObj = getTime(date);
       user[index].date = dateObj;
-      res.json(user[index]);
     } else {
-      res.json(user);
       console.log("id is not in the temp");
     }
+    return res.json(user[index]);
   } catch (err) {
     console.log(err);
     res.json("id not founded, server error");
