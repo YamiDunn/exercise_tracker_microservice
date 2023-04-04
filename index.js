17024aa9a67dd29d7859f87bca3cbc0cddbe7ef9
YamiDunn <j.felipe.rodriguez@hotmail.com>
2023-04-04 16:28:43 -0500

    -Test, exercise creation

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const {nanoid} = require("nanoid");
+const { nanoid } = require("nanoid");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
 
 app.use(cors());
 app.use("/public", express.static(`${process.cwd()}/public`));
@@ -23,17 +22,70 @@ app.listen(port, function () {
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
-})
-
-const user = [{ username: String, _id: Number }];
-
-app.route("/api/users").post((req, res, next) => {
-  let userData = req.body.username;
-  let gId = nanoid(20);
-  user.push({username: userData, _id: gId });
-  return res.json(user);
 });
 
-/*).get((req, res) => {
-  res.json(user);
-}*/
+const user = [{ username: "TestMain, _id", _id: 00000000000000000000 }];
+const getTime = (date) => {
+  let dateObj;
+  let validDate =
+    /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/.test(
+      date
+    );
+  if (date) {
+    if (validDate) {
+      dateObj = new Date(date);
+      dateObj =
+        dateObj.getFullYear() +
+        "-" +
+        (dateObj.getMonth() + 1) +
+        "-" +
+        (dateObj.getDate() + 1);
+      console.log("you are here", dateObj);
+    } else {
+      console.log("invalid date input");
+    }
+  } else {
+    date = new Date();
+    dateObj =
+      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  }
+  return dateObj;
+};
+
+app
+  .route("/api/users")
+  .post((req, res, next) => {
+    let userData = req.body.username;
+    let gId = nanoid(20);
+    var cUser = { username: userData, _id: gId };
+    user.push(cUser);
+    res.json({ username: userData, _id: gId });
+    next();
+  })
+  .get((req, res) => {
+    res.json(user);
+  });
+
+app.route("/api/users/:_id/exercises").post((req, res) => {
+  let uid = req.body[":_id"];
+  let desc = req.body.description;
+  let dur = req.body.duration;
+  let date = req.body.date;
+
+  try {
+    let index = user.findIndex((el) => el._id == uid);
+    if (index) {
+      user[index].description = desc;
+      user[index].duration = dur;
+      let dateObj = getTime(date);
+      user[index].date = dateObj;
+      res.json(user[index]);
+    } else {
+      res.json(user);
+      console.log("id is not in the temp");
+    }
+  } catch (err) {
+    console.log(err);
+    res.json("id not founded, server error");
+  }
+});
